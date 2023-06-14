b19707cf4a8701bb5eaf0896ae5f49aac398cb76
Ahmed-sherifhe1 <115046140+Ahmed-sherifhe1@users.noreply.github.com>
2023-06-14 18:36:36 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+let image1=document.getElementById("image1")
+let image2=document.getElementById("image2")
+let image3=document.getElementById("image3")
+let image4=document.getElementById("image4")
+let changer=document.getElementById("changer")
+let image5=document.getElementById("image5")
+let image6=document.getElementById("image6")
+let image7=document.getElementById("image7")
+let image8=document.getElementById("image8")
+image1.onclick=function(){
+    changer.src=image1.src;
+}
+image2.onclick=function(){
+    changer.src=image2.src;
+}
+image3.onclick=function(){
+    changer.src=image3.src;
+}
+image4.onclick=function(){
+    changer.src=image4.src;
+}
+image5.onmouseover=function(){
+    changer.src=image5.src;
+}
+image6.onmouseover=function(){
+    changer.src=image6.src;
+}
+image7.onmouseover=function(){
+    changer.src=image7.src;
+}
+image8.onmouseover=function(){
+    changer.src=image8.src;
+}
